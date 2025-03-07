8f99fbeeb491ea08aee69655cb5bf33189966587
Shwetal More <more.shwetal777@gmail.com>
2025-03-08 00:02:44 +0530

    Hyperparameter Tunning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -51,9 +51,50 @@ class ModelTrainer:
                 "CatBoost Regressor": CatBoostRegressor(verbose=False)
             }
 
+            params={
+                "Decision Tree Regressor": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest Regressor":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting Regressor":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{
+                    'fit_intercept':[True,False],
+                },
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoost Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "K-Neighbors Regressor":{
+                    'n_neighbors': [3,5,7,9,11,13]
+                }
+            }
+
             model_report:dict=evaluate_models(X_train=X_train, y_train=y_train, 
                                              X_test=X_test, y_test=y_test, 
-                                             models=models)
+                                             models=models, params=params)
             
             # To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 import dill
 from src.exception import CustomException
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import r2_score
 
 
@@ -19,14 +19,22 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
 
         for i in range(len(list(models))):
-            model = list(models.values())[i]
 
-            model.fit(X_train, y_train) # Train model
+            model = list(models.values())[i]
+            param = params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model, param_grid=param, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+
+            # model.fit(X_train, y_train) # Train model
 
             y_train_pred = model.predict(X_train) # Make predictions
 
